64f6f3a96763595161a362d37b9711aa87f89986
Fardin Ahmed <niloy98.it@gmail.com>
2020-04-15 05:09:19 +0600

    create a base page to display allocated budget information

---
+++ user/budget.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+$wardNumber = $_SESSION["wardNumber"];
+
+if (empty($_SESSION["username"])) {
+    header("Location: ../index.php"); // Redirecting To Home Page
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<header>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</header>
+<link rel="stylesheet" href="../css/main.css">
+
+<body>
+    <!--       navigation menu -->
+    <?php include('userNav.html') ?>
+   
+    <?php 
+    
+    $conn = new mysqli("localhost", "root", "", "connectcouncillor");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM budget WHERE `wardNumber`='$wardNumber'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+          $title = $row['budgetTitle'];
+          $amount = $row['amount'];
+          $description = $row['description'];
+          echo "<h3>Title:  $title </h3>";
+          echo "<h4>Amount:  $amount </h4>";
+          echo "<h4>Description:  $description </h4>";
+          
+        }
+    } else {
+        $error = "Username or Password is invalid";
+    }
+    $conn->close();
+    
+    ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
