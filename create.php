52db4f790834b0863e08d02f3e06d0a610dcfcfa
Fardin Ahmed <niloy98.it@gmail.com>
2020-05-16 04:52:35 +0600

    added validation

--- create.php
+++ create.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </header>
 <link rel="stylesheet" href="../css/main.css">
 
@@ -19,12 +18,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-7 offset-2">
-                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                    <form method="post" name="create" onsubmit="return validateform()" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
                         <form>
                             <div class="form-row">
                                 <div class="form-group col-md-7">
                                     <label for="inputEmail4">Name</label>
-                                    <input type="Name" class="form-control" placeholder="Name" name="name">
+                                    <input type="Name" class="form-control" onkeypress="return isChar(event)" placeholder="Name" name="name">
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputVoterID">Blood Group</label>
@@ -32,11 +31,11 @@
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputVoterID">Age</label>
-                                    <input type="text" class="form-control" placeholder="Age" name="age">
+                                    <input type="text" class="form-control" onkeypress="return isNumber(event)" placeholder="Age" name="age">
                                 </div>
                                 <div class="form-group col-md-7">
                                     <label for="inputVoterID">Voter ID</label>
-                                    <input type="text" class="form-control" placeholder="Voter ID" name="vid">
+                                    <input type="text" class="form-control" onkeypress="return isNumber(event)" placeholder="Voter ID" name="vid">
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputEmail4">Username</label>
@@ -56,8 +55,9 @@
                                 <div class="form-group col-md-4">
                                     <label for="inputState">City</label>
                                     <select class="form-control" name="city">
-                                        <option selected>Choose...</option>
-                                        <option>...</option>
+                                        <option selected>Dhaka</option>
+                                        <option>Chittagong</option>
+                                        <option>Rajshahi</option>
                                     </select>
                                 </div>
                                 <div class="form-group col-md-4">
@@ -76,12 +76,22 @@
             </div>
         </div>
     </section>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Already have an account?</p>
+                    <a href="index.php">Login</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!--          code to upload image  -->
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $conn = new mysqli("localhost", "root", "", "connectcouncillor");
-
-        echo "connected";
 
         // If file upload form is submitted
         $name = $_REQUEST["name"];
@@ -118,14 +128,12 @@
                     $insert = $conn->query("INSERT into userinfo (`name`,`userName`, `age`, `bloodgroup`, `address`,`ward`,`userImage`,`voterid`) VALUES ('$name','$uname','$age','$bloodgroup', '$address','$ward','$imgContent','$voterID')");
 
                     $insert = $conn->query("INSERT INTO `login`(`username`, `password`) VALUES ('$uname','$password')");
-                    
-                    
+
+
 
                     if ($insert) {
                         $status = 'success';
                         $statusMsg = "user created successfully.";
-                        
-                    
                     } else {
                         $statusMsg = "user creation failed, please try again.";
                     }
@@ -141,17 +149,81 @@
         echo $statusMsg;
     }
     ?>
-    <!--        user statstic section-->
+
+    <script>
+        function isNumber(evt) {
+            evt = (evt) ? evt : window.event;
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+            }
+            return true;
+        }
+
+        function isChar(evt) {
+            evt = (evt) ? evt : window.event;
+            var charCode = (evt.which) ? evt.which : evt.keyCode;
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return true;
+            }
+            return false;
+        }
+
+        function validateform() {
+
+
+
+            var name = document.create.name.value;
+            var blood = document.create.blood.value;
+            var age = document.create.age.value;
+            var vid = document.create.vid.value;
+            var uname = document.create.uname.value;
+            var password = document.create.password.value;
+            var address = document.create.address.value;
+            var city = document.create.city.value;
+            var wardnumber = document.create.wardnumber.value;
+
+
+
+            if (name == null || name == "") {
+                alert("name can't be blank");
+                return false;
+            } else if (blood == null || blood == "") {
+                alert("blood can't be blank");
+                return false;
+            } else if (age == null || age == "") {
+                alert("age can't be blank");
+                return false;
+            } else if (vid == null || vid == "") {
+                alert("voter id can't be blank");
+                return false;
+            } else if (uname == null || uname == "") {
+                alert("username can't be blank");
+                return false;
+            } else if (password == null || password == "") {
+                alert("password can't be blank");
+                return false;
+            } else if (address == null || address == "") {
+                alert("address can't be blank");
+                return false;
+            } else if (city == null || city == "") {
+                alert("city can't be blank");
+                return false;
+            } else if (wardnumber == null || wardnumber == "") {
+                alert("ward number can't be blank");
+                return false;
+            }
+        }
+    </script>
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
 
